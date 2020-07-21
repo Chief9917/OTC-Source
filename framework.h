e396fdca64661a3e9d9069577c2d4bb7c320f0aa
Chief9917 <68563327+Chief9917@users.noreply.github.com>
2020-07-21 09:45:47 +0300

    Add files via upload

---
+++ framework.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+#include <Windows.h>
+#include <iterator>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <psapi.h>
+#include <map>
+
+#include "headers/utils/Utils.h"
+#include "libs/headers/HookLib.h"
+#include "headers/runtime/Logger.h"
+#include "headers/segment/Segment.h"
+#include "headers/utils/PanicUtils.h"
+#include "headers/frameworks/GameFramework.h"
+#include "headers/frameworks/SegmentFramework.h"
+
+#pragma comment (lib, "libs/libs/HookLib.lib")
